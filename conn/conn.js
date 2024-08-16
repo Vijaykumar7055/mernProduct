4a6d7bfbcb742d2e6d12bb819f857d72be472f6d
Vijaykumar7055 <131260351+Vijaykumar7055@users.noreply.github.com>
2024-08-16 20:55:05 +0530

    changed

--- conn/conn.js
+++ conn/conn.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 const conn = async ()=>{
-try {
-    await mongoose.connect(process.env.URI);
-    console.log("contteted to database")
-} catch (error) {
-    console.log(error)
+    try {
+        await mongoose.connect(process.env.URI);
+        console.log('MongoDB connected...');
+      } catch (err) {
+        console.error(err.message);
+        // Exit process with failure
+        process.exit(1);
+      }
 
-}
 };
 
 conn();
