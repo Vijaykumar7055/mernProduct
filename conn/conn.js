fc1b476f655413c9d0d4f0a7776a64666b2fe8f0
Vijaykumar7055 <131260351+Vijaykumar7055@users.noreply.github.com>
2025-03-25 22:06:30 +0530

    Add files via upload

---
+++ conn/conn.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const conn = async ()=>{
+    try {
+        await mongoose.connect(`${process.env.URI}`);
+        console.log('MongoDB connected...');
+      } catch (err) {
+        console.error(err.message);
+        // Exit process with failure
+        process.exit(1);
+      }
+
+};
+
+conn();
